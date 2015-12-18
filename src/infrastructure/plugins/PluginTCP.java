ab81aacf066773f4b3afc95c3f257c15d01a1c94
Ruan <ruan.gouveia@gmail.com>
2015-12-18 16:34:47 -0300

    Plugin HTTP (incompleto)

--- src/infrastructure/plugins/PluginTCP.java
+++ src/infrastructure/plugins/PluginTCP.java
@@ -6,20 +6,23 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import infrastructure.Plugin;
+
 public class PluginTCP extends Plugin {
+
 	private boolean isServer;
 	private String host = null;
 	private int port;
-	
+
 	private Socket clientSocket;
 	private ServerSocket welcomeSocket = null;
-	
+
 	private DataOutputStream out=null;
 	private DataInputStream in=null;
-		
+
 	private int sentMessageSize;
 	private int receiveMessageSize;
-	
+
 
 	public PluginTCP(String host, int port, boolean isServer) throws IOException {
 		super();
@@ -29,53 +32,61 @@ public class PluginTCP extends Plugin {
 	}
 
 	public void send(byte[] msg) throws IOException {
-		
-		//se for cliente, abre conexao socket
-		if (!isServer) {
-			clientSocket = new Socket(this.host, this.port);
-			out = new DataOutputStream(clientSocket.getOutputStream());
-			in = new DataInputStream(clientSocket.getInputStream());
-		}
-		
-		//prepera os dados para o envio 
-		sentMessageSize = msg.length;
-		out.writeInt(sentMessageSize);
-		out.write(msg, 0, sentMessageSize);
-		out.flush(); //forca a escrita no stream 
-		
-		//quando for servidor, fecha os streams e o socket aqui 
-		if(isServer) {
-			clientSocket.close();
-			welcomeSocket.close();
-			out.close();
-			in.close();
+
+		try{
+			//se for cliente, abre conexao socket
+			if (!isServer) {
+				clientSocket = new Socket(this.host, this.port);
+				out = new DataOutputStream(clientSocket.getOutputStream());
+				in = new DataInputStream(clientSocket.getInputStream());
+			}
+
+			//prepera os dados para o envio 
+			sentMessageSize = msg.length;
+			out.writeInt(sentMessageSize);
+			out.write(msg, 0, sentMessageSize);
+			out.flush(); //forca a escrita no stream 
+
+			//quando for servidor, fecha os streams e o socket aqui 
+			if(isServer) {
+				clientSocket.close();
+				welcomeSocket.close();
+				out.close();
+				in.close();
+			}
+		}catch(IOException e){
+			System.err.println(e.getMessage() + "\n" + e.getStackTrace());
 		}
 		return;
 	}
 
 	public byte[] receive() throws IOException {
 		
-		//se for servidor, cria um servidor socket 
-		if (isServer) {
-			welcomeSocket = new ServerSocket(port);
-			clientSocket = welcomeSocket.accept(); 
-			out = new DataOutputStream(clientSocket.getOutputStream());
-			in = new DataInputStream(clientSocket.getInputStream());
-		}
-
-		//preprara para receber os dados 
 		byte[] msg = null;		
-		receiveMessageSize = in.readInt();
-		msg = new byte[receiveMessageSize];
-		in.read(msg, 0, receiveMessageSize);
-		
-		//quando for cliente, fecha os streams e o socket aqui 
-		if (!isServer) {
-			clientSocket.close();
-			out.close();
-			in.close();
-		}
 
+		try{
+			//se for servidor, cria um servidor socket 
+			if (isServer) {
+				welcomeSocket = new ServerSocket(port);
+				clientSocket = welcomeSocket.accept(); 
+				out = new DataOutputStream(clientSocket.getOutputStream());
+				in = new DataInputStream(clientSocket.getInputStream());
+			}
+
+			//preprara para receber os dados 
+			receiveMessageSize = in.readInt();
+			msg = new byte[receiveMessageSize];
+			in.read(msg, 0, receiveMessageSize);
+
+			//quando for cliente, fecha os streams e o socket aqui 
+			if (!isServer) {
+				clientSocket.close();
+				out.close();
+				in.close();
+			}
+		}catch(IOException e){
+			System.err.println(e.getMessage() + "\n" + e.getStackTrace());
+		}
 		return msg;
 	}
 
--- src/infrastructure/plugins/PluginUDP.java
+++ src/infrastructure/plugins/PluginUDP.java
@@ -6,15 +6,21 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+<<<<<<< HEAD
+import distribution.Message;
+import infrastructure.Plugin;
+
+=======
+>>>>>>> 8ba00ac75d25449c0bd7fc18765e0e12e105182f
 public class PluginUDP extends Plugin {
 
 	private DatagramSocket s = null;
 	private DatagramPacket recievePackege = null;
-	
+
 	private boolean isServer;
 	private int port;
 	private String host;
-	
+
 	private int sentMessageSize;
 	private int receiveMessageSize;
 
@@ -27,52 +33,95 @@ public class PluginUDP extends Plugin {
 
 	@Override
 	public void send(byte[] msg) throws UnknownHostException, IOException {
+
 		DatagramPacket req = null;
-		
-		//se for cliente, abre conexao socket
-		if (!isServer) {
-			s = new DatagramSocket();
-			InetAddress serv = InetAddress.getByName(host);
-			
-			//caso seja cliente, vai enviar pra o ip e a porta passado
-			req = new DatagramPacket(msg, msg.length, serv, port);
-		} else {
-			
-			//caso seja servidor, vai enviar o packege de volta ao endereco recebido 
-			req = new DatagramPacket(msg, msg.length, recievePackege.getAddress(), recievePackege.getPort());
+
+		try{
+			byte[] msgSize = String.valueOf(msg.length).getBytes();
+
+			// se for cliente, abre conexao socket
+			if (!isServer) {
+				s = new DatagramSocket();
+				InetAddress serv = InetAddress.getByName(host);
+
+				// envia o tamanho da mensagem
+				DatagramPacket req2 = new DatagramPacket(String.valueOf(msg.length).getBytes(), msgSize.length, serv, port);
+				s.send(req2);
+
+				// caso seja cliente, vai enviar pra o ip e a porta passado
+				req = new DatagramPacket(msg, msg.length, serv, port);
+
+			} else {
+				//envia o tamanho da mensagem
+				DatagramPacket req2 = new DatagramPacket(String.valueOf(msg.length).getBytes(), msgSize.length,
+						recievePackege.getAddress(), recievePackege.getPort());
+
+				s.send(req2);
+				// caso seja servidor, vai enviar o packege de volta ao endereco
+				// recebido
+				req = new DatagramPacket(msg, msg.length, recievePackege.getAddress(), recievePackege.getPort());
+			}
+
+			// envia datagrama contendo a mensagem
+			s.send(req);
+
+			// s.setSoTimeout(10000); // timeout em ms
+
+			// Servidor fecha a conexao aqui
+			if (isServer)
+				s.close();
+		}catch(IOException e){
+			System.err.println(e.getMessage() + "\n" + e.getStackTrace());
 		}
-
-		// envia datagrama contendo a mensagem 
-		s.send(req);
-
-		// s.setSoTimeout(10000); // timeout em ms
-
-		//Servidor fecha a conexao aqui 
-		if (isServer)
-			s.close();
 
 	}
 
 	@Override
 	public byte[] receive() throws IOException {
+
 		// cria um socket UDP
 		if (isServer)
 			s = new DatagramSocket(port);
-		
+
 		byte[] buffer = new byte[10000];
 		byte[] msg = null;
 
-		// cria datagrama para receber request do cliente
-		recievePackege = new DatagramPacket(buffer, buffer.length);
-		s.receive(recievePackege);
+		try{
+			// cria datagrama para receber request do cliente
+			recievePackege = new DatagramPacket(buffer, buffer.length);
+			s.receive(recievePackege);
+			String message = new String(recievePackege.getData());
 
-		// envia resposta
-		msg = recievePackege.getData();
-		
-		//cliente fecha a conexao aqui 
-		if (!isServer)
-			s.close();
+
+			sentMessageSize = trimForInt(message);
+			buffer = new byte[sentMessageSize];
+
+			s.receive(recievePackege);
+
+			// envia resposta
+			msg = recievePackege.getData();
+
+			// cliente fecha a conexao aqui
+			if (!isServer)
+				s.close();
+		}catch(IOException e){
+			System.err.println(e.getMessage() + "\n" + e.getStackTrace());
+		}
 		return msg;
+	}
+
+	public int trimForInt (String message) {
+		StringBuffer sb = new StringBuffer();
+
+		char[] caracteres = message.toCharArray();
+
+		for (Character caracter : caracteres) {
+			if (Character.isDigit(caracter)) {
+				sb.append(caracter);
+			}
+		}
+
+		return Integer.valueOf(sb.toString());
 	}
 
 	public int getSentMessageSize() {
