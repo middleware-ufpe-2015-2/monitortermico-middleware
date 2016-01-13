9a79091a418098a3ae76aab05cb3866b248db616
Fagner Fernandes Candido da Silva <ffcandido@trf5.jus.br>
2016-01-13 10:22:34 -0300

    inclusao das alteracoes no QosObserver.

--- src/infrastructure/qosobserver/QosObserver.java
+++ src/infrastructure/qosobserver/QosObserver.java
@@ -12,71 +12,63 @@ public class QosObserver implements IQosObserver {
 
 	public QosObserver() {
 	}
-	
+
 	@Override
-	public Calendar tempo1() throws IOException, InterruptedException, IOException {
-		 
+	public Calendar tempo1() throws IOException, InterruptedException,
+			IOException {
+
 		SimpleDateFormat sdf = new SimpleDateFormat("HHmmssSSS");
-		//Pega o tempo 1, salva no arquivo e retorna para o Invoker
+		// Pega o tempo 1, salva no arquivo e retorna para o Invoker
 		Date hora = Calendar.getInstance().getTime();
-		//alterando o formato da data e hora 
+		// alterando o formato da data e hora
 		String hr = sdf.format(hora);
 		Calendar date = Calendar.getInstance();
-		//Salvar o "hora" no arquivo
-		BufferedWriter buffWrite = new BufferedWriter(new FileWriter("../qos_timetable.txt",true));
+		// Salvar o "hora" no arquivo
+		BufferedWriter buffWrite = new BufferedWriter(new FileWriter(
+				"../qos_timetable.txt", true));
 		buffWrite.append(hr + "\n");
 		buffWrite.close();
-		
+
 		return date;
 	}
 
 	@Override
-	//Recebe o tempo 1 para comparar com o 2 e resultar em um tempo total
-	public void tempo2(Calendar date) throws IOException, InterruptedException, IOException{
-		SimpleDateFormat sdf = new SimpleDateFormat("HHmmssSSS");  
+	// Recebe o tempo 1 para comparar com o 2 e resultar em um tempo total
+	public void tempo2(Calendar date) throws IOException, InterruptedException,
+			IOException {
+		SimpleDateFormat sdf = new SimpleDateFormat("HHmmssSSS");
 		SimpleDateFormat sdf2 = new SimpleDateFormat("HHmmssSSS");
-		
-		//Pega o tempo 2 e salva no arquivo
+
+		// Pega o tempo 2 e salva no arquivo
 		Date hora2 = Calendar.getInstance().getTime();
-		//alterando o formato da data e hora 
+		// alterando o formato da data e hora
 		String hr2 = sdf.format(hora2);
-		Calendar endDate = Calendar.getInstance();  
+		Calendar endDate = Calendar.getInstance();
 		try {
 			endDate.setTime(sdf2.parse(hr2));
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		//Salvar o "hora2" no arquivo
-		BufferedWriter buffWrite2 = new BufferedWriter(new FileWriter("../qos_timetable.txt",true));
+		// Salvar o "hora2" no arquivo
+		BufferedWriter buffWrite2 = new BufferedWriter(new FileWriter(
+				"../qos_timetable.txt", true));
 		String linha = sdf.format(hora2);
 		buffWrite2.append(linha + "\n");
-		buffWrite2.close();	
-		
-		//Pega o tempo 2 e diminui do tempo 1
-		//Resultado sai em Milisegundos
-		endDate.add(Calendar.HOUR_OF_DAY, -date.get(Calendar.HOUR_OF_DAY));  
-		endDate.add(Calendar.MINUTE, -date.get(Calendar.MINUTE));  
-		endDate.add(Calendar.SECOND, -date.get(Calendar.SECOND));  
-		endDate.add(Calendar.MILLISECOND, -date.get(Calendar.MILLISECOND));  
-		
-		/**
-		 * Função para ler arquivo texto (pode ser útil em futuras funcionalidades
-		 * 
-		 * BufferedReader buffRead = new BufferedReader(new FileReader("../qos_timetable.txt"));
-		String linha2 = "";
-		while (true) {
-			if (linha2 != null) {
-				System.out.println(linha2);
-			} else
-				break;
-			linha2 = buffRead.readLine();
-		}
-		buffRead.close();
-		
-		 */
+		buffWrite2.close();
+
+		// Pega o tempo 2 e diminui do tempo 1
+		// Resultado sai em Milisegundos
+		endDate.add(Calendar.HOUR_OF_DAY, -date.get(Calendar.HOUR_OF_DAY));
+		endDate.add(Calendar.MINUTE, -date.get(Calendar.MINUTE));
+		endDate.add(Calendar.SECOND, -date.get(Calendar.SECOND));
+		endDate.add(Calendar.MILLISECOND, -date.get(Calendar.MILLISECOND));
 
 		String endTime = sdf.format(endDate.getTime());
 		int tempoTotal = Integer.parseInt(endTime);
-		//resultado em inteiro para colocar no gráfico
+		// resultado em inteiro para colocar no gráfico
+		BufferedWriter buffWrite3 = new BufferedWriter(new FileWriter(
+				"../qos_timetable2.txt", true));
+		buffWrite3.append(tempoTotal + "\n");
+		buffWrite3.close();
 	}
 }
---
+++ src/infrastructure/qosobserver/QosObserverTxtReader.java
@@ -0,0 +1,51 @@
+package infrastructure.qosobserver;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import javax.swing.JOptionPane;
+
+public class QosObserverTxtReader {
+
+	public static void main(String[] args) {
+		String mostra = "";
+		String nomeArq = "../qos_timetable2.txt"; // Nome do arquivo, pode ser
+													// absoluto, ou pastas
+													// /dir/teste.txt
+		String linha = "";
+		File arq = new File(nomeArq);
+
+		// Arquivo existe
+		if (arq.exists()) {
+			mostra = "Arquivo ' " + nomeArq + " ' , aberto com sucesso! \n";
+			// mostra+="Tamanho do arquivo "+Long.toString(arq.length())+"\n";
+			// tentando ler arquivo
+			try {
+				mostra = mostra + "\nMedições:\n\n";
+				// abrindo arquivo para leitura
+				FileReader reader = new FileReader(nomeArq);
+				// leitor do arquivo
+				BufferedReader leitor = new BufferedReader(reader);
+
+				while (true) {
+					linha = leitor.readLine();
+					if (linha == null) {
+						break;
+					}
+					mostra += linha + " milissegundos" + "\n";
+				}
+				leitor.close();
+			} catch (Exception erro) {
+			}
+
+			JOptionPane.showMessageDialog(null, mostra,
+					"Tempos totais das medições", 1);
+		}
+		// Se nao existir
+		else
+			JOptionPane.showMessageDialog(null, "Arquivo não existe!", "Erro",
+					0);
+	}
+
+}
